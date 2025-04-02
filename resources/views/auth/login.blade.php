c010aa4b6edcf815be0cb4483d4b544c2cfc4e1a
jeansamist <jeansamist@gmail.com>
2025-04-02 16:16:47 +0100

    login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,47 +4,68 @@
 @section('title', 'Login Page')
 
 @section('content')
-
-
-    <div class="row justify-content-center">
-            <div class="col-md-4">
-                    <p>Bienvenue dans notre plateforme de gestion des notes. Connecter vous pour consulter vos resultats</p>
-                <div class="card shadow-sm">
-                    <div class="card-header text-center fw-bold">
-                        Login
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ url('/login') }}">
-                            @csrf
-
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul class="mb-0">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email address</label>
-                                <input type="email" name="email" class="form-control" id="email"
-                                       value="{{ old('email') }}" required autofocus>
-                            </div>
-
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" name="password" class="form-control" id="password" required>
-                            </div>
-
-                            <div class="d-grid">
-                                <button type="submit" class="btn btn-primary">Log in</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
+<section class="p-3 p-md-4 p-xl-5">
+  <div class="container">
+    <div class="card border-light-subtle shadow-sm">
+      <div class="row g-0">
+        <div class="col-12 col-md-6 text-bg-primary">
+          <div class="d-flex align-items-center justify-content-center h-100">
+            <div class="col-10 col-xl-8 py-3">
+              <h2 class="h1 mb-4">Bienvenue dans notre plateforme de gestion des notes.</h2>
+              <p class="lead m-0">Connectez-vous pour consulter vos résultats.</p>
             </div>
+          </div>
         </div>
+        <div class="col-12 col-md-6">
+          <div class="card-body p-3 p-md-4 p-xl-5">
+            <div class="row">
+              <div class="col-12">
+                <div class="mb-5">
+                  <h3>Log in</h3>
+                </div>
+              </div>
+            </div>
+            <form method="POST" action="{{ url('/login') }}">
+              @csrf
+              
+              @if ($errors->any())
+                <div class="alert alert-danger">
+                  <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                    @endforeach
+                  </ul>
+                </div>
+              @endif
+              
+              <div class="row gy-3 gy-md-4 overflow-hidden">
+                <div class="col-12">
+                  <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+                  <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" value="{{ old('email') }}" required autofocus>
+                </div>
+                <div class="col-12">
+                  <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+                  <input type="password" class="form-control" name="password" id="password" required>
+                </div>
+                <div class="col-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
+                    <label class="form-check-label text-secondary" for="remember_me">
+                      Keep me logged in
+                    </label>
+                  </div>
+                </div>
+                <div class="col-12">
+                  <div class="d-grid">
+                    <button class="btn btn-xl btn-primary" type="submit">Log in now</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
